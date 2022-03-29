084f8f15ff8c63eb36b8174b7b7fbadbb159e1ee
Alvin Mufidha Ahmad <79785202+AlvinMoved13@users.noreply.github.com>
2022-03-29 11:44:17 +0700

    Add files via upload

---
+++ Latihan pratikum minggu ke 4/Pratikum/inputradio.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>Pilih Jurusan</title></head>
+<body>
+<FORM ACTION="prosesradio.php" METHOD="POST" NAME="input">
+<h2>Pilih Jurusan Anda :</h2>
+<input type="radio" name="jurusan" value="A11" checked>
+Teknik Informatika<br>
+<input type="radio" name="jurusan" value="A12"> Sistem
+Informasi<br>
+<input type="radio" name="jurusan" value="A22"> Sistem
+Komputer<br>
+<input type="submit" name="Pilih" value="Pilih">
+</FORM>
+</body>
+</html>
---
+++ Latihan pratikum minggu ke 4/Pratikum/prosescombobox.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_POST['Pilih'])) {
+$film = $_POST['kartun'];
+echo "Fakultas Anda adalah :
+<font color=blue><b>$film</b></font>";
+}
+?>
