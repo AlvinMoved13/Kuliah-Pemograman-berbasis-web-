c6449ee2b5e36ede02e3892b49a9853c47e76052
Alvin Mufidha Ahmad <79785202+AlvinMoved13@users.noreply.github.com>
2022-04-06 15:10:20 +0700

    Delete 4April - Latihan 5/Latihan directory

--- 4April - Latihan 5/Latihan/4April - Latihan 5.php
+++
@@ -1,9 +0,0 @@
-#Membuat tabel
-CREATE TABLE nama_tabel (field-1 type (Size), field-2 type
-(Size));
-#Membuat Kunci Primer
-CREATE TABLE nama_tabel ( field-1 type(length)PRIMARY KEY, field2 type(length), …… ….(….));
-#Membuat Kolom Unik
-CREATE TABLE nama_tabel ( field-1 type(length), field-2
-type(length), …… ….(….),UNIQUE (field-1,field-2));
-
